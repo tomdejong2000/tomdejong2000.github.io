a0bdd31479e58f07bf76ab9ed13a6512b875c6de
tomdejong2000 <tomdejong2000@gmail.com>
2023-04-05 00:17:50 +0200

    fixed nav + added contact

--- js/script.js
+++ js/script.js
@@ -19,22 +19,27 @@ $( document ).ready(function() {
     $('.aboutmelink').hover(function() {
         $('.expendbar').toggleClass("expended")
         $('.expendtext').text("aboutme")
+        $('.navbar').toggleClass("nobg")
     })
     $('.schoollink').hover(function() {
         $('.expendbar').toggleClass("expended")
         $('.expendtext').text("school projects")
+        $('.navbar').toggleClass("nobg")
     })
     $('.personallink').hover(function() {
         $('.expendbar').toggleClass("expended")
         $('.expendtext').text("personal projects")
+        $('.navbar').toggleClass("nobg")
     })
     $('.timelink').hover(function() {
         $('.expendbar').toggleClass("expended")
         $('.expendtext').text("timelapse")
+        $('.navbar').toggleClass("nobg")
     })
     $('.contactlink').hover(function() {
         $('.expendbar').toggleClass("expended")
         $('.expendtext').text("contact me")
+        $('.navbar').toggleClass("nobg")
     })
 
 
